79c5bbd257b0956f86f0f80e4e37c5d865a815d8
rofiatul khoiriyah <khoiriyahdemak03@gmail.com>
2020-02-05 17:53:01 +0700

    perubahan 3

--- app/src/main/java/com/rovic/intent/MainActivity.kt
+++ app/src/main/java/com/rovic/intent/MainActivity.kt
@@ -2,14 +2,19 @@ package com.rovic.intent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.sdk27.coroutines.onClick
+import org.jetbrains.anko.startActivity
+import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        //LATIHAN GIT
-        //0000000000
+        button.onClick {
+            startActivity<DetailActivity>()
+            toast("Berhasil")
+        }
     }
 }
